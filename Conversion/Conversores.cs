3c9e3017bc77229f222466ee67ff152e84713a2f
matheusrv0 <rondonfamiliav@gmail.com>
2025-04-08 15:04:51 -0300

    Adicionar arquivos de projeto.

---
+++ Conversion/Conversores.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion
+{
+    public class Conversores
+    {
+        public static double cParaF(double C)
+        {
+            return (C * 1.8 + 32);
+
+        }
+        public static double CParaK(double C)
+        {
+            return (C + 273.15);
+
+        }
+        public static double FParaC(double F)
+        {
+            return (F - 32) * 5 / 9;
+        }
+        public static double FParaK(double F)
+        {
+            return (F - 32) * 5 / 9 + 273.15;
+        }
+        public static double KParaC(double K)
+        {
+            return (K - 273.15);
+        }
+        public static double KParaF(double K)
+        {
+            return (K - 273.15) * 9 / 5 + 32;
+        }
+
+    }
+}
---
+++ Conversion/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Conversion
+{
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("----------------Conversor de temperatura----------");
+            Console.WriteLine("Digite a temperatura: ");
+            double temperatura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Escolha a opção de conversão: ");
+            Console.WriteLine("1 - Celsius");
+            Console.WriteLine("2 - Farenheit");
+            Console.WriteLine("3 - Kelvin");
+                int opcao = Convert.ToInt32(Console.ReadLine());
+
+                    switch (opcao)
+                    {
+                        case 1:
+                            Console.WriteLine("Celsius para Farenheit: " + Conversores.cParaF(temperatura));
+                            Console.WriteLine("Celsius para Kelvin: " + Conversores.CParaK(temperatura));
+                            break;
+                        case 2:
+                            Console.WriteLine("Farenheit para Celsius: " + Conversores.FParaC(temperatura));
+                            Console.WriteLine("Farenheit paraKelvin: " + Conversores.FParaK(temperatura));
+                            break;
+                        case 3:
+                            Console.WriteLine("Kelvin para Celsius: " + Conversores.KParaC(temperatura));
+                            Console.WriteLine("Kelvin para Farenheit: " + Conversores.KParaF(temperatura));
+                            break;
+                        default:
+                            Console.WriteLine("Opção Invalida");
+                           break;
+
+            }
+        }
+
+
+    }
+}
